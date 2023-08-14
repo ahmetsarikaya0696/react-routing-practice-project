1dd44bbca3d311bd996058d23b443aaf14cb95b4
ahmet sarikaya <ahmetsarikaya0696@outlook.com>
2023-08-14 10:17:45 +0300

    MainNavigation Error.js ' ten kaldırıldı.

--- frontend/src/pages/Error.js
+++ frontend/src/pages/Error.js
@@ -1,6 +1,5 @@
 import { useRouteError } from "react-router-dom";
 import PageContent from "../components/PageContent";
-import MainNavigation from "../components/MainNavigation";
 
 const ErrorPage = () => {
   const error = useRouteError();
@@ -18,7 +17,6 @@ const ErrorPage = () => {
 
   return (
     <>
-      <MainNavigation />
       <PageContent title={title}>{message}</PageContent>
     </>
   );
